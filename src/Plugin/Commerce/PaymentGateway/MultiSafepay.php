145b7740dd63169988e980c29f8d5919c0d9eb14
Thomas Cys <thomas.cys@telenet.be>
2017-09-19 21:22:47 +0200

    Initial commit

---
+++ src/Plugin/Commerce/PaymentGateway/MultiSafepay.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\commerce_multisafepay\Plugin\Commerce;
+
+
+use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\OffsitePaymentGatewayBase;
+
+/**
+ * Class MultiSafepayGateway
+ *
+ * @package Drupal\commerce_multisafepay\Plugin\Commerce
+ */
+class MultiSafepayGateway extends OffsitePaymentGatewayBase {
+
+}
---
+++ src/PluginForm/OffsiteForm/MultiSafepayPaymentOffsiteForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\commerce_multisafepay\PluginForm\OffsiteForm;
+
+
+use Drupal\commerce_payment\PluginForm\PaymentOffsiteForm;
+
+/**
+ * Class MultiSafepayPaymentOffsiteForm
+ *
+ * @package Drupal\commerce_multisafepay\PluginForm\OffsiteForm
+ */
+class MultiSafepayPaymentOffsiteForm extends PaymentOffsiteForm {
+
+}
