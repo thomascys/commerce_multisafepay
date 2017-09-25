e9c56dbd8f4447b884bc7f5f791427956bcac55b
Thomas Cys <thomas.cys@telenet.be>
2017-09-25 22:53:15 +0200

    Moved payment validation from onReturn to onNotify

--- src/Plugin/Commerce/PaymentGateway/MultiSafepay.php
+++ src/Plugin/Commerce/PaymentGateway/MultiSafepay.php
@@ -3,13 +3,17 @@
 namespace Drupal\commerce_multisafepay\Plugin\Commerce\PaymentGateway;
 
 
+use Drupal\commerce_multisafepay\MultiSafepayClientInterface;
 use Drupal\commerce_order\Entity\OrderInterface;
 use Drupal\commerce_payment\PaymentMethodTypeManager;
 use Drupal\commerce_payment\PaymentTypeManager;
 use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\OffsitePaymentGatewayBase;
+use Drupal\commerce_price\Price;
 use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -25,6 +29,12 @@ use Symfony\Component\HttpFoundation\Request;
  * )
  */
 class MultiSafepay extends OffsitePaymentGatewayBase {
+
+  /**
+   * @var \Drupal\commerce_multisafepay\MultiSafepayClient
+   */
+  protected $multiSafepayClient;
+
   /**
    * MultiSafepayGateway constructor.
    *
@@ -36,8 +46,26 @@ class MultiSafepay extends OffsitePaymentGatewayBase {
    * @param \Drupal\commerce_payment\PaymentMethodTypeManager $payment_method_type_manager
    * @param \Drupal\Component\Datetime\TimeInterface $time
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, PaymentTypeManager $payment_type_manager, PaymentMethodTypeManager $payment_method_type_manager, TimeInterface $time) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, PaymentTypeManager $payment_type_manager, PaymentMethodTypeManager $payment_method_type_manager, MultiSafepayClientInterface $multisafepay_client, TimeInterface $time) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $payment_type_manager, $payment_method_type_manager, $time);
+    $this->multiSafepayClient = $multisafepay_client;
+    $this->multiSafepayClient->setOptions($configuration['api_key'], $configuration['mode']);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('plugin.manager.commerce_payment_type'),
+      $container->get('plugin.manager.commerce_payment_method_type'),
+      $container->get('commerce_multisafepay.client'),
+      $container->get('datetime.time')
+    );
   }
 
   /**
@@ -84,23 +112,55 @@ class MultiSafepay extends OffsitePaymentGatewayBase {
    * @param \Drupal\commerce_order\Entity\OrderInterface $order
    * @param \Symfony\Component\HttpFoundation\Request $request
    */
-  public function onReturn(OrderInterface $order, Request $request) {
-    parent::onReturn($order, $request);
-  }
-
-  /**
-   * @param \Drupal\commerce_order\Entity\OrderInterface $order
-   * @param \Symfony\Component\HttpFoundation\Request $request
-   */
   public function onCancel(OrderInterface $order, Request $request) {
     parent::onCancel($order, $request);
   }
 
   /**
    * @param \Symfony\Component\HttpFoundation\Request $request
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
    */
   public function onNotify(Request $request) {
-    parent::onNotify($request);
-  }
+    $remote_id = $request->get('transactionid');
+    // Return early if there's no transaction id.
+    if (empty($remote_id)) {
+      return new JsonResponse('Error 500', 500);
+    }
+    // Load the remote order.
+    $remote_order = $this->multiSafepayClient->loadOrder($remote_id);
 
+    // Try to load the payment entity.
+    /** @var \Drupal\commerce_payment\PaymentStorage $payment_storage */
+    $payment_storage = $this->entityTypeManager->getStorage('commerce_payment');
+    /** @var \Drupal\commerce_payment\Entity\Payment $payment */
+    $payment = $payment_storage->loadByRemoteId($remote_id);
+    if ($payment) {
+      if ($remote_order['data']['status'] === 'completed') {
+        $payment->setState('capture_completed');
+      }
+      $payment->setRemoteState($remote_order['data']['status']);
+      $payment->save();
+
+      return new JsonResponse('OK');
+    }
+    else {
+      // Create a new payment.
+      $amount = $remote_order['data']['amount'];
+      $currency = $remote_order['data']['currency'];
+      $order_id = $remote_order['data']['order_id'];
+
+      $payment = $payment_storage->create([
+        'state' => 'authorization',
+        'amount' => new Price($amount, $currency),
+        'payment_gateway' => $this->entityId,
+        'order_id' => $order_id,
+        'remote_id' => $remote_id,
+      ]);
+      $payment->save();
+
+      return new JsonResponse('OK');
+    }
+
+  }
 }
